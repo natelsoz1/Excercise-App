ffa49a39653db5a983bf4f1c6206f9feb08a05a6
natesoz1 <thezanman42@gmail.com>
2018-10-17 14:09:53 -0400

    Added Express

---
+++ index.js
@@ -0,0 +1,12 @@
+const port = 3000;
+const http = require('http');
+const server = http.createServer((req, res) => {
+    if (req.url == '/') {
+        //---
+    }
+
+    if (req.url == '/api') {
+        //---
+    }
+});
+server.listen(port);
