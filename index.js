0c3886b1f2c2953165264a5e3e8c656bb409037a
natesoz1 <thezanman42@gmail.com>
2018-10-17 17:02:51 -0400

    Fixed small changes

--- index.js
+++ index.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const app = express();
-
-app.use(express.json());
-
-class User{
-    constructor(name){
-    this.name = name;
-    }
-}
+const user = require('./model');
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hellow World!!!');
-});
-
-app.get('/api/courses', (req, res) => {
-    res.send([1, 2, 3]);
 });
 
 app.post('/user', function(req, res, next) {
