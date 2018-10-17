45af903641d4f0885dc631eabd4b576f1e9e5e55
natesoz1 <thezanman42@gmail.com>
2018-10-17 17:00:33 -0400

    Added User class

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+class User{
+    constructor(name){
+    this.name = name;
+    }
+}
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
     res.send('Hellow World!!!');
 });
@@ -8,6 +18,13 @@ app.get('/', (req, res) => {
 app.get('/api/courses', (req, res) => {
     res.send([1, 2, 3]);
 });
+
+app.post('/user', function(req, res, next) {
+    const user = new User(req.body.name);
+    user.push(user);
+    res.send(user);
+    });
+
 
 const port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
---
+++ model.js
@@ -0,0 +1,7 @@
+
+class User{
+    constructor(name, exercise){
+        this.exercise = exercise;
+        this.name = name;
+    }
+}
