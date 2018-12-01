a00d7a5df3db6a1978b89fb4c4df06b9807db281
natesoz1 <thezanman42@gmail.com>
2018-12-01 15:11:46 -0500

    Attempting to use API and VUE

---
+++ src/components/api_access.js
@@ -0,0 +1,53 @@
+const api_root = process.env.VUE_APP_API_ROOT;
+
+export function GetState(){
+    return myFetch(api_root + "/");
+}
+
+export function Register(){
+    return myFetch(api_root + "./Server/controller.js");
+}
+
+export function generate(name,firstName, lastName, password){
+    return myFetch(api_root + "/generate", {name,firstName, lastName, password});
+}
+export function FlipPicture(){
+    return myFetch(api_root + "/picture", {})
+}
+
+export function SubmitCaption(c){
+    return myFetch(api_root + "/playedCaptions", {text: c})
+}
+export function ChooseCaption(c){
+    return myFetch(api_root + "/playedCaptions/choose",  {text: c.text})
+}
+
+
+
+
+
+  function myFetch(url = ``, data = null) {
+      let options = {
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, same-origin, *omit
+            headers: {
+                playerId: 0
+            }
+      };
+      if(data){
+          options = { 
+            ...options,
+            method:  "POST", // *GET, POST, PUT, DELETE, etc.
+            headers: {
+                ...options.headers,
+                "Content-Type": "application/json; charset=utf-8",
+                // "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: JSON.stringify(data), // body data type must match "Content-Type" header
+          };
+      }
+      return fetch(url, options)
+      .then(response =>{
+        return response.json()
+      }); // parses response to JSON
+  }
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,10 +4,41 @@
         A journey of a thaousand miles beings with a single step.
     </div>
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Login
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="basic-addon1">Userame</span>
+  </div>
+  <input type="text" id ="name" class="form-control" placeholder="Userame" aria-label="Userame" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="basic-addon1">First Name</span>
+  </div>
+  <input type="text" id ="firstname" class="form-control" placeholder="First Name" aria-label="First Name" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="basic-addon1">Last Name</span>
+  </div>
+  <input type="text" id ="lastname" class="form-control" placeholder="Last Name" aria-label="Last Name" aria-describedby="basic-addon1">
+</div>
+
+<div class="input-group mb-3">
+  <div class="input-group-prepend">
+    <span class="input-group-text" id="basic-addon1">Password</span>
+  </div>
+  <input type="text" id ="password" class="form-control" placeholder="Password" aria-label="Password" aria-describedby="basic-addon1">
+</div>
+
+<button type="button" class="btn btn-primary" @click.prevent= "register()" >
+ Register
 </button>
+
+
+
+
 
 <!-- Modal -->
 <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
@@ -34,13 +65,20 @@
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import api from '@/components/api_access';
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  
+  methods: {
+    
+    register(){
+      var name = document.getElementById("name").value
+      var firstname = document.getElementById("firstname").value 
+      var lastname = document.getElementById("lastname").value 
+      var password = document.getElementById("password").value
+        api.register(name, firstname, lastname, password);
+    },
+    
   }
 }
 </script>
